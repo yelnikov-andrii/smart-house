b70b487d9f95bb204b6015aae1aa8a8759feb0dc
Andrii Yelnikov <whiteyelnikov@gmail.com>
2023-10-21 20:28:48 +0300

    added anchors to links

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,34 @@ langUaLink.addEventListener("click", () => {
   langUaLink.classList.add("header__language--active");
 });
 
+function isNumericInput(event) {
+  const inputValue = event.target.value;
+  return !isNaN(inputValue) || inputValue === '+';
+}
+
+function onPhoneInput(event) {
+  if (!isNumericInput(event)) {
+    return;
+  }
+}
+
+function onPhoneInput1(event) {
+  if (!isNumericInput(event)) {
+    return;
+  }
+}
+
+document.getElementById("phone").addEventListener("input", onPhoneInput);
+document.getElementById("phone1").addEventListener("input", onPhoneInput1);
+
+function isValidPhoneNumber(phoneNumber) {
+  const internationalPhoneRegex = /^\+380\d{9}$/;
+
+  const localPhoneRegex = /^(0\d{9}|09\d{8}|06\d{8})$/;
+
+  return internationalPhoneRegex.test(phoneNumber) || localPhoneRegex.test(phoneNumber);
+}
+
 function handleSubmit(event) {
   event.preventDefault();
 
@@ -19,10 +47,19 @@ function handleSubmit(event) {
   let usernameInput1 = document.getElementById("name1");
   let cellphoneInput1 = document.getElementById("phone1");
 
-  console.log(usernameInput.value);
-  console.log(cellphoneInput.value);
-  console.log(usernameInput1.value);
-  console.log(cellphoneInput1.value);
+  if (isValidPhoneNumber(cellphoneInput.value)) {
+    console.log(usernameInput.value);
+    console.log(cellphoneInput.value);
+  } else {
+    console.error("Некорректный номер телефона!");
+  }
+
+  if (isValidPhoneNumber(cellphoneInput1.value)) {
+    console.log(usernameInput1.value);
+    console.log(cellphoneInput1.value);
+  } else {
+    console.error("Некорректный номер телефона!");
+  }
 
   usernameInput.value = "";
   cellphoneInput.value = "";
